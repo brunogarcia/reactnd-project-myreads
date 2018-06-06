9e22fdbe134bdf7f1647d2b83605845b156a128d
Bruno Garcia <bruno@garciaechegaray.com>
2018-06-06 17:29:34 +0200

    feat: made a call to API, searching for books and the save response into component state

--- src/container/Search/index.js
+++ src/container/Search/index.js
@@ -1,28 +1,86 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import constants from '../../utils/constants';
-// import * as BooksAPI from '../../BooksAPI'
+import Error from '../../components/Error';
+import * as BooksAPI from '../../BooksAPI';
 
 const { MAIN } = constants.APP.PATH;
+const MIN_LENGTH_SEARCH = 5;
 
 class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
       query: '',
+      loading: false,
+      error: false,
       books: [],
     };
 
     this.handleChange = this.handleChange.bind(this);
   }
 
+  componentDidMount() {
+    this.isAlreadyMounted = true;
+  }
+
+  componentWillUnmount() {
+    this.isAlreadyMounted = false;
+  }
+
   handleChange(e) {
     const { value } = e.target;
-    this.setState({ query: value });
+    this.setState({
+      query: value,
+    }, this.checkMinLength);
+  }
+
+  checkMinLength() {
+    const { query } = this.state;
+
+    if (query.length < MIN_LENGTH_SEARCH) {
+      this.setState({
+        books: [],
+      });
+    }
+
+    if (query.length >= MIN_LENGTH_SEARCH) {
+      this.searchBooks(query);
+    }
+  }
+
+  searchBooks(query) {
+    BooksAPI.search(query)
+      .then((books) => {
+        if (this.isAlreadyMounted) {
+          this.setState({
+            books,
+            loading: false,
+          });
+        }
+      }).catch(() => {
+        if (this.isAlreadyMounted) {
+          this.setState({
+            error: true,
+          });
+        }
+      });
   }
 
   render() {
-    const { query } = this.state;
+    const {
+      error,
+      loading,
+      query,
+      books,
+    } = this.state;
+
+    if (error) {
+      return <Error />;
+    }
+
+    console.log(books);
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
