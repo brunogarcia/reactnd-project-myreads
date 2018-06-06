ed56b166644b7110afb82c71d98a3e6e83a78f1c
Bruno Garcia <bruno@garciaechegaray.com>
2018-06-06 16:31:35 +0200

    feat: add a react-router Link to AddBook

--- src/components/AddBook/index.js
+++ src/components/AddBook/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import constants from '../../utils/constants';
+
+const { SEARCH } = constants.APP.PATH;
 
 const AddBook = () => (
   <div className="open-search">
-    <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+    <Link to={SEARCH}>Add a book</Link>
   </div>
 );
 
