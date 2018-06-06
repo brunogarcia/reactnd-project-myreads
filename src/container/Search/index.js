a39ce04dda4ebfd3ab74f320c1650cfa2c055b3a
Bruno Garcia <bruno@garciaechegaray.com>
2018-06-06 16:49:59 +0200

    feat: add event listener to search text field and save the value on the component state

--- src/container/Search/index.js
+++ src/container/Search/index.js
@@ -9,26 +9,31 @@ class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      query: '',
       books: [],
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const { value } = e.target;
+    this.setState({ query: value });
   }
 
   render() {
+    const { query } = this.state;
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link to={MAIN} className="close-search" >Close</Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author.
-              So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              value={query}
+              onChange={this.handleChange}
+              placeholder="Search by title or author"
+            />
           </div>
         </div>
         <div className="search-books-results">
