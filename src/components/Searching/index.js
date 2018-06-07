9169f5d896e6d16f0a6ee57a8d1a721ac5498b4f
Bruno Garcia <bruno@garciaechegaray.com>
2018-06-07 19:07:19 +0200

    refactor: remove Searching component - It will be replace by a generic component that shows messages.

--- src/components/Searching/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Searching = () => (
-  <div className="Searching-main">
-    <p className="search-searching">Searching...</p>
-  </div>
-);
-
-export default Searching;
