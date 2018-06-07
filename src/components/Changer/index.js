6c1301273996f1877ea79a27d8a571a4855db658
Bruno Garcia <bruno@garciaechegaray.com>
2018-06-07 17:23:30 +0200

    fix: Changer component - The default value of the select must be the current shelf

--- src/components/Changer/index.js
+++ src/components/Changer/index.js
@@ -4,13 +4,13 @@ import randomID from 'random-id';
 import Types from '../../utils/types';
 import constants from '../../utils/constants';
 
-const items = Object.entries(constants.SHELFS);
+const shelfs = Object.entries(constants.SHELFS);
 
 const Book = (props) => {
   const { shelf, onChange } = props;
 
   const renderOptions = () => (
-    items.map(([key, value]) => (
+    shelfs.map(([key, value]) => (
       <option key={randomID()} value={key}>
         {value}
       </option>
@@ -24,9 +24,9 @@ const Book = (props) => {
 
   return (
     <div className="book-shelf-changer">
-      <select defaultValue="disabled" onChange={handleChange}>
+      <select defaultValue={shelf} onChange={handleChange}>
         <option value="disabled" disabled>Move to...</option>
-        {renderOptions(shelf)}
+        {renderOptions()}
       </select>
     </div>
   );
