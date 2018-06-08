df5703820c0ad54acb3c2552fdcfd1a10b8fed99
Bruno Garcia <bruno@garciaechegaray.com>
2018-06-08 13:35:10 +0200

    feat: all books for search page must have a proper shelf - When the component is mounted, make a call to BookAPI.getAll() - With the response, create a map with the ID and shelf of each book. - When the user searches for a topic, it needed  to check the response with the map created before, looking for a match between response and user's books. - If there is a match, must replace the default shelf (none)  by the proper shelf. - Finally, save the response on the state.

--- src/container/Search/index.js
+++ src/container/Search/index.js
@@ -4,17 +4,28 @@ import * as BooksAPI from '../../BooksAPI';
 import Error from '../../components/Error';
 import SearchBar from '../../components/SearchBar';
 import SearchResults from '../../components/SearchResults';
+import Loading from '../../components/Loading';
 import constants from '../../utils/constants';
 
 const { MIN_LENGTH, MESSAGES } = constants.SEARCH;
+
+/**
+ * Create a map of my bookshelves
+ * @param books: collection of book objects
+ * @returns collection of user's books objects with this structure {id: '12345', shelf: 'read'}
+ */
+const createMapOfMyBookshelves = books => (
+  books.reduce((acc, { id, shelf }) => (acc.concat({ id, shelf })), [])
+);
 
 class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
       message: '',
+      loading: true,
       error: false,
-      books: [],
+      results: [],
     };
 
     this.handleChangeSearch = this.handleChangeSearch.bind(this);
@@ -23,17 +34,48 @@ class Search extends Component {
 
   componentDidMount() {
     this.isAlreadyMounted = true;
+    this.getAllBooks();
   }
 
   componentWillUnmount() {
     this.isAlreadyMounted = false;
   }
 
+  getAllBooks() {
+    BooksAPI.getAll()
+      .then((response) => {
+        if (this.isAlreadyMounted) {
+          this.myBookshelves = createMapOfMyBookshelves(response);
+          this.setState({
+            loading: false,
+          });
+        }
+      }).catch(() => {
+        if (this.isAlreadyMounted) {
+          this.setState({
+            error: true,
+          });
+        }
+      });
+  }
+
+  getIndexOnMyBookshelves(id) {
+    return this.myBookshelves.findIndex(book => book.id === id);
+  }
+
+  getBookShelf(bookId) {
+    const index = this.getIndexOnMyBookshelves(bookId);
+    const noResults = (index === -1);
+
+    return noResults ?
+      constants.SHELVES.none.toLowerCase() :
+      this.myBookshelves[index].shelf;
+  }
+
   handleChangeShelf(book, shelf) {
     BooksAPI.update(book, shelf)
       .then((response) => {
         if (!isEmpty(response) && this.isAlreadyMounted) {
-          console.log(response);
           this.setState({
             message: MESSAGES.shelfChanged,
           });
@@ -50,7 +92,7 @@ class Search extends Component {
   handleChangeSearch(query) {
     if (query.length < MIN_LENGTH) {
       this.setState({
-        books: [],
+        results: [],
         message: '',
       });
     } else {
@@ -58,20 +100,33 @@ class Search extends Component {
     }
   }
 
-  checkAPIResponse(response) {
+  checkSearchResponse(response) {
     const { error } = response;
 
     if (error) {
       this.setState({
-        books: [],
+        results: [],
         message: MESSAGES.noResults,
       });
     } else {
       this.setState({
-        books: response,
         message: '',
+        results: this.addShelvesToBooks(response),
       });
     }
+  }
+
+  /**
+   * Add a proper shelf to each book
+   * @param response: collection of book objects
+   * @return collection of book with a proper shelf assigned
+   */
+  addShelvesToBooks(books) {
+    return books.map((book) => {
+      const clonedBook = { ...book };
+      clonedBook.shelf = this.getBookShelf(book.id);
+      return clonedBook;
+    });
   }
 
   searchBooks(query) {
@@ -82,8 +137,7 @@ class Search extends Component {
     BooksAPI.search(query)
       .then((response) => {
         if (this.isAlreadyMounted) {
-          console.log(response);
-          this.checkAPIResponse(response);
+          this.checkSearchResponse(response);
         }
       }).catch(() => {
         if (this.isAlreadyMounted) {
@@ -97,8 +151,9 @@ class Search extends Component {
   render() {
     const {
       error,
+      loading,
       message,
-      books,
+      results,
     } = this.state;
 
     if (error) {
@@ -107,11 +162,12 @@ class Search extends Component {
 
     return (
       <div className="search-books">
+        <Loading isLoading={loading} />
         <SearchBar
           onChangeSearch={this.handleChangeSearch}
         />
         <SearchResults
-          books={books}
+          results={results}
           message={message}
           onChangeShelf={this.handleChangeShelf}
         />
