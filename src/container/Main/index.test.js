51bb050388809c8251720c709724e9f104081a54
Bruno Garcia <bruno@garciaechegaray.com>
2018-06-08 18:31:11 +0200

    test: Main component unit tests

--- src/container/Main/index.test.js
+++ src/container/Main/index.test.js
@@ -1,18 +1,55 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Main from './index';
+import Header from '../../components/Header';
+import Bookshelves from '../../components/Bookshelves';
+import AddBook from '../../components/AddBook';
 import Loading from '../../components/Loading';
 import Error from '../../components/Error';
 
-describe('Main states', () => {
+const props = {
+  history: {
+    push: () => {},
+  },
+};
+
+const books = [
+  {
+    title: 'test 1',
+    authors: ['test 1', 'test 1'],
+    imageLinks: {
+      smallThumbnail: 'test 1',
+      thumbnail: 'test 1',
+    },
+  },
+  {
+    title: 'test 2',
+    authors: ['test 2', 'test 2'],
+    imageLinks: {
+      smallThumbnail: 'test 2',
+      thumbnail: 'test 2',
+    },
+  },
+];
+
+describe('Main component states', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Main {...props} />);
+  });
+
   it('renders Loading component with default state', () => {
-    const wrapper = shallow(<Main />);
+    wrapper.setState({
+      loading: true,
+      error: false,
+      books: [],
+    });
+
     expect(wrapper.find(Loading).exists()).toBeTruthy();
   });
 
-  it('renders Error component with some error', () => {
-    const wrapper = shallow(<Main />);
-
+  it('renders Error component if found some error', () => {
     wrapper.setState({
       loading: false,
       error: true,
@@ -23,14 +60,40 @@ describe('Main states', () => {
   });
 });
 
-describe('Main lifecycle', () => {
+describe('Main component lifecycle', () => {
   it('should call getAllBooks when the component did mount', () => {
     const getAllBooksMocked = jest.spyOn(Main.prototype, 'getAllBooks');
 
-    shallow(<Main />);
+    shallow(<Main {...props} />);
 
     expect(getAllBooksMocked).toHaveBeenCalledTimes(1);
 
     getAllBooksMocked.mockClear();
+  });
+});
+
+describe('Main component has books', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Main {...props} />);
+
+    wrapper.setState({
+      loading: false,
+      error: false,
+      books,
+    });
+  });
+
+  it('renders Header component', () => {
+    expect(wrapper.find(Header).exists()).toBeTruthy();
+  });
+
+  it('renders Bookshelves component', () => {
+    expect(wrapper.find(Bookshelves).exists()).toBeTruthy();
+  });
+
+  it('renders AddBook component', () => {
+    expect(wrapper.find(AddBook).exists()).toBeTruthy();
   });
 });
