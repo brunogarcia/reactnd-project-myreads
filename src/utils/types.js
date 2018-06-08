a373a48194ebb6b4fa9e1b0273c4b391b03cb269
Bruno Garcia <bruno@garciaechegaray.com>
2018-06-08 13:14:46 +0200

    refactor: create a couple of new types for shelf (id, title)

--- src/utils/types.js
+++ src/utils/types.js
@@ -1,7 +1,8 @@
 import { oneOf, shape, arrayOf, string } from 'prop-types';
 import constants from './constants';
 
-const shelf = oneOf(Object.keys(constants.SHELVES));
+const shelfID = oneOf(Object.keys(constants.SHELVES));
+const shelfTitle = oneOf(Object.values(constants.SHELVES));
 
 const book = shape({
   title: string.isRequired,
@@ -19,4 +20,9 @@ const shelves = shape({
   currentlyReading: string.isRequired,
 });
 
-export default { book, shelf, shelves };
+export default {
+  book,
+  shelfID,
+  shelfTitle,
+  shelves,
+};
